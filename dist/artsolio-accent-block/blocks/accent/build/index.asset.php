c1c246760f374a79e1082b11310e9757241dd442
rpk1000 <110998505+rpk1000@users.noreply.github.com>
2025-10-22 20:07:32 -0700

    Add editor-only styles

--- dist/artsolio-accent-block/blocks/accent/build/index.asset.php
+++ dist/artsolio-accent-block/blocks/accent/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '839afd3a1e0c9f5e2d06');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '4359d9a446bdce638b3d');
